3bc798f86c5e55155ba85e2d8a37ffd6d63e526e
Vaibhavi-Sita <52885102+Vaibhavi-Sita@users.noreply.github.com>
2023-02-08 19:17:15 +0530

    firebase update

---
+++ image-uploader/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCi-5qMcCYlkdtW9HzTRZMXgFnIxmsbtT4",
+  authDomain: "image-uploader-3a50c.firebaseapp.com",
+  projectId: "image-uploader-3a50c",
+  storageBucket: "image-uploader-3a50c.appspot.com",
+  messagingSenderId: "634627534214",
+  appId: "1:634627534214:web:2806fe9b81f566fdb51007"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
