abf85dd020cb4b911f2c5066371478fea30b5072
Vaibhavi-Sita <52885102+Vaibhavi-Sita@users.noreply.github.com>
2023-02-11 02:39:04 +0530

    clean code and update repo

--- image-uploader/src/components/button/button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export function Button({ onClick, children }) {
-  return (
-    <button className='button' onClick={onClick}>
-        {children}
-    </button>
-  )
-}
